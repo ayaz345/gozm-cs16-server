4c477764125fcca86db4e21c4a262c7971b5b060
DmitriyS <DSavin@yotateam.com>
2014-04-02 04:19:21 +0300

    amxbans_web:sync

--- AmxBans/top.php
+++ AmxBans/top.php
@@ -19,9 +19,11 @@
             #header('Content-Type: text/html; charset=windows-1251');
             #setlocale(LC_ALL, 'ru_RU.CP1251', 'rus_RUS.CP1251', 'Russian_Russia.1251', 'russian');
 
-            $resource = mysql_query("SELECT `nick`, `zombiekills`, `infect`, `death`, `infected`, `rank`, `extra` FROM (SELECT *, (@_c := @_c + 1) AS `rank`, ((`infect` + `zombiekills`*2 + `humankills` + `extra`) / (`infected` + `death` + 300)) AS `skill` FROM (SELECT @_c := 0) r, `zp_players` ORDER BY `skill` DESC) AS `newtable` WHERE `rank` <= 100 ORDER BY `rank` LIMIT 100;");
+            $resource = mysql_query("SELECT `nick`, `zombiekills`, `infect`, `death`, `infected`, `rank`, `extra` FROM (SELECT *, (@_c := @_c + 1) AS `rank`, ((`infect` + `zombiekills`*2 + `humankills` + `extra`) / (`infected` + `death` + 300)) AS `skill` FROM (SELECT @_c := 0) r, `zp_players` ORDER BY `skill` DESC) AS `newtable` WHERE `rank` <= 100 ORDER BY `rank` LIMIT 42;");
 
-            echo '<table border="1"><tr><th>Rank</th><th>Nick</th><th>ZM killed</th><th>Infects</th><th>Infected</th><th>Deaths</th><th>Bonuses</th><th>Total Skill</th></tr>';
+			echo '<table><tr><td>';
+
+            echo '<table border=1 style="float:left;margin-right:50px;"><tr><th>Rank</th><th>Nick</th><th>ZM killed</th><th>Infects</th><th>Infected</th><th>Deaths</th><th>Bonuses</th><th>Total Skill</th></tr>';
             while ($row = mysql_fetch_object($resource))
             {
                 $skill_float = ($row->zombiekills*2 + $row->infect + $row->extra) / ($row->death + $row->infected + 300);
@@ -29,6 +31,22 @@
                 echo "<tr><td>" . $row->rank . ". </td><td>" . $row->nick . "</td><td>" . $row->zombiekills . "</td><td>" . $row->infect . "</td><td>" . $row->infected . "</td><td>" . $row->death . "</td><td>" . $row->extra . "</td><td> " . $skill . " </td></tr>";
             }
             echo '</table>';
+
+			echo '</td><td>';
+
+			$resource = mysql_query("SELECT `map`, `games` from `zp_maps` ORDER BY `games` DESC;");
+			echo '<table border=1 style="float:right;margin-left:50px;"><tr><th>No.</th><th>Map</th><th>Games</th></tr>';
+			$iter = 1;
+            while ($row = mysql_fetch_object($resource))
+            {
+                $map = $row->map;
+                $games = $row->games;
+                echo "<tr><td>" . $iter . " </td><td>" . $map . " </td><td>" . $games . "</td></tr>";
+				$iter = $iter + 1;
+            }
+            echo '</table>';
+
+			echo '</td></tr></table>';
         ?>
     </body>
 </html>
